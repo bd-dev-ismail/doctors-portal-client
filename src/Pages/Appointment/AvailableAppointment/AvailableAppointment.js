501b043bd55d147df21b756ab6f6d1c0b1ff1c72
bd-dev-ismail <ismailratul7@gmail.com>
2022-11-13 05:21:15 +0600

    dynamically load modal data

--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,9 +1,11 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOptions from './AppointmentOptions';
 
 const AvailableAppointment = ({ selectedDate }) => {
   const [appointmentOptions, setAppointmentOptions] = useState([]);
+  const [treatment, setTreatment] = useState(null);
   useEffect(()=>{
     fetch("appointmentOptions.json")
     .then(res => res.json())
@@ -14,14 +16,16 @@ const AvailableAppointment = ({ selectedDate }) => {
       <p className="text-center text-secondary font-bold">
         Available Appointments on {format(selectedDate, "PP")}
       </p>
-      <div className='grid grid-cols-1 mt-6 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+      <div className="grid grid-cols-1 mt-6 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {appointmentOptions.map((option) => (
           <AppointmentOptions
             key={option._id}
             appointmentOption={option}
+            setTreatment={setTreatment}
           ></AppointmentOptions>
         ))}
       </div>
+      {treatment && <BookingModal treatment={treatment}></BookingModal>}
     </section>
   );
 };
